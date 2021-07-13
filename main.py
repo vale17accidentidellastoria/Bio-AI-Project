7c14d11ee65b659c804014ac1bf4c7ea8ac938b4
vale17accidentidellastoria <pingupucca@gmail.com>
2021-07-13 23:53:25 +0200

    improved indexes print with tabular printing

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import os
 import cities_co2_graph as transport_graph
 import st_index as st_index
 import pr_index as pr_index
+import wr_index as wr_index
 from PIL import Image
 import prices as prices
 
+from tabulate import tabulate
 
 from pylab import *
 
@@ -18,13 +20,12 @@ from inspyred_utils import NumpyRandomWrapper
 import multi_objective
 
 from functools import reduce
-import wr_index as wr_index
 
-# Disable
+# Disable print
 def blockPrint():
     sys.stdout = open(os.devnull, 'w')
 
-# Restore
+# Restore print
 def enablePrint():
     sys.stdout = sys.__stdout__
 
@@ -40,46 +41,36 @@ def main(argv):
         elif opt in ['-f', '--transport_data_file']:
             TRANSPORT_DATA_FILE = arg
 
-    #blockPrint()
+    blockPrint()
     #Create transport graph
     G, shortest_paths, co2_path_lengths, edge_attrs, countries = transport_graph.main(['-d', DESTINATION_COUNTRY, '-f', TRANSPORT_DATA_FILE])
-    #enablePrint()
+    enablePrint()
 
-    print("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°")
-    print("The sustainability indexes for transport are:")
+    data_to_print = []
 
     #create an empty vector for a dictionary with countries as keys and sustainability indexes as values
     st_index_countries = {}
     wr_index_countries = {}
+    pr_index_countries, final_table = pr_index.compute_production_sustainability_index()
+
+    prices_countries = prices.get_prices()
 
     for c in countries:
         if c == DESTINATION_COUNTRY:
             st_index_countries[c] = 10.0
-            wr_index_countries[c] = wr_index.compute_workers_sustainability_index(c)
         else:
             st_index_countries[c] = st_index.compute_transport_sustainability_index(G, shortest_paths[c], edge_attrs)
-            wr_index_countries[c] = wr_index.compute_workers_sustainability_index(c)
-        print("\t {} = {}".format(c, st_index_countries[c]))
-        print("workers index: ", c, wr_index_countries[c])
 
-    pr_index_countries, final_table = pr_index.compute_production_sustainability_index()
+        wr_index_countries[c] = wr_index.compute_workers_sustainability_index(c)
 
-    print("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°")
-    print("The sustainability indexes for production are:")
-    for c in pr_index_countries.keys():
-        print("\t {} = {}".format(c, pr_index_countries[c]))
-    print("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°")
-    print("Corresponding csv file has been saved in the \'data\' directory.")
+        data_to_print.append([c, pr_index_countries[c], wr_index_countries[c], st_index_countries[c], prices_countries[c]])
+
+    print(tabulate(data_to_print, headers=["Country", "Production", "Workers", "Transport", "Cotton Price (€)"]))
+
+    #print("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°")
+    #print("Corresponding csv file has been saved in the \'data\' directory.")
     with Image.open('data/final.png') as img:
         img.show()
-
-
-    prices_countries = prices.get_prices()
-
-    print("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°")
-    print("Average cotton prices per country are:")
-    for c in prices_countries.keys():
-        print("\t {} = {}{}".format(c, prices_countries[c], "€"))
 
 
 if __name__ == "__main__":
--- prices.py
+++ prices.py
@@ -1,13 +1,13 @@
 
 def get_prices():
-    prices = {"Cina": 2.44,
+    prices = {"China": 2.44,
     "Bangladesh": 2.03,
-    "America": 3.04,
+    "USA": 3.04,
     "Vietnam":1.68,
     "India":2.36,
     "Uzbekistan":1.68,
-    "Perù":2.28,
-    "Turchia":2.19,
-    "Egitto":3.37,
-    "Italia":3.21}
+    "Peru":2.28,
+    "Turkey":2.19,
+    "Egypt":3.37,
+    "Italy":3.21}
     return prices 
